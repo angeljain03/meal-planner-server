83d6f13c1f6e76c865d7c501d35c2a5504885549
angeljain03 <008.angeljain@gmail.com>
2023-12-19 23:55:24 +0530

    check user existence

--- app/controllers/api/auth/auth_controller.rb
+++ app/controllers/api/auth/auth_controller.rb
@@ -1,16 +1,17 @@
 class Api::Auth::AuthController < ApplicationController
   def login
-    user = User.find_by(username: params[:username])
+    user = User.find_by(:username => params[:username])
 
     # check if user exists
     if !user
-      render json:{ msg: "Username doesn't exists" , user: user} , status: :unprocessable_entity
-    end
-
-    if user.authenticate(params[:password])
-      render json:{ msg: 'Success Login' , user: user} , status: :ok
+      render json: { msg: "Username doesn't exists" , user: user} , status: :unprocessable_entity;
     else
-      render json:{ msg: 'Wrong Password'} , status: :unauthorized
+
+      if user.authenticate(params[:password])
+        render json:{ msg: 'Success Login' , user: user} , status: :ok
+      else
+      render json:{ msg: 'Wrong Password'} , status: :unprocessable_entity
+      end
     end
   end
 end
